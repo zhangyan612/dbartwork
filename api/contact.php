f4825ebd26c30ac200e10db6fae7706447e7b24f
Yan Zhang <zhangyan612@gmail.com>
2018-03-28 00:32:00 -0500

    Update contact php

--- api/contact.php
+++ api/contact.php
@@ -11,20 +11,30 @@ if(empty($_POST['name'])  		||
 	return false;
    }
 	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-	
 
-$from = new SendGrid\Email("Admin", "admin@yanzhangit.com");
-$subject = "Diana Barron Artwork Website Contact Form: $name"; 
-$to = new SendGrid\Email("Yan Zhang", "zhangyan612@gmail.com");
-$content = new SendGrid\Content("text/plain", "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-$response = $sg->client->mail()->send()->post($mail);
+try {
 
-//mail($to,$email_subject,$email_body,$headers);
-return true;			
+   $name = $_POST['name'];
+   $email_address = $_POST['email'];
+   $message = $_POST['message'];
+      
+
+   $from = new SendGrid\Email("Admin", "admin@yanzhangit.com");
+   $subject = "Diana Barron Artwork Website Contact Form: $name"; 
+   $to = new SendGrid\Email("Yan Zhang", "zhangyan612@gmail.com");
+
+   $content = new SendGrid\Content("text/plain", "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message");
+
+   $mail = new SendGrid\Mail($from, $subject, $to, $content);
+   $apiKey = getenv('SENDGRID_API_KEY');
+   $sg = new \SendGrid($apiKey);
+   $response = $sg->client->mail()->send()->post($mail);
+
+   return true;   
+
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+    return $e->getMessage();
+}
+		
 ?>
